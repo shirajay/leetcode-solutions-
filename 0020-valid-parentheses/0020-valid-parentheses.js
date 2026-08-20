8587726a75df2c76a19ad4b8524e0b1d7fded176
Ajay Shir <shirajay40@gmail.com>
2026-08-20 16:31:10 +0530

    Time: 2 ms (83.58%), Space: 57.3 MB (21.56%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,23 @@
+var isValid = function(s) {
+    let stack = [];
+
+    for (let char of s) {
+
+        if (char === '(') {
+            stack.push(')');
+        } 
+        else if (char === '[') {
+            stack.push(']');
+        } 
+        else if (char === '{') {
+            stack.push('}');
+        } 
+        else {
+            if (stack.pop() !== char) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
