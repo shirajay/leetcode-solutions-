1eabc7e11f37e0f1859b570a00a8f8fac4a9c704
Ajay Shir <shirajay40@gmail.com>
2026-08-30 19:49:24 +0530

    Time: 1 ms (46.46%), Space: 53.5 MB (70.33%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,18 @@
+var strStr = function(haystack, needle) {
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        let j = 0;
+
+        while (
+            j < needle.length &&
+            haystack[i + j] === needle[j]
+        ) {
+            j++;
+        }
+
+        if (j === needle.length) {
+            return i;
+        }
+    }
+
+    return -1;
+};
