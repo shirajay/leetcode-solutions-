e89542a6b129af968fcf81aef4849be78532760c
Ajay Shir <shirajay40@gmail.com>
2026-09-11 08:34:01 +0530

    Time: 0 ms (100%), Space: 52.5 MB (94.2%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,15 @@
+var plusOne = function(digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+
+        digits[i] = 0;
+    }
+
+    // All digits were 9
+    digits.unshift(1);
+
+    return digits;
+};
