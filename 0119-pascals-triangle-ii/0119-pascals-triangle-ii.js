230bc83e425e85ed3b9cab06ad99aa7d29152ae2
Ajay Shir <shirajay40@gmail.com>
2026-09-15 07:53:08 +0530

    Time: 0 ms (100%), Space: 53.4 MB (68.31%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -0,0 +1,13 @@
+var getRow = function(rowIndex) {
+    let row = [1];
+
+    for (let i = 1; i <= rowIndex; i++) {
+        row.push(1);
+
+        for (let j = i - 1; j > 0; j--) {
+            row[j] = row[j] + row[j - 1];
+        }
+    }
+
+    return row;
+};
